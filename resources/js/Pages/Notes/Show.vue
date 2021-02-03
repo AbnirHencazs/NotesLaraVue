ea216068124eff83445a0bfd67ddfc4cffb93eee
BrianSanchez@Accesa <briansanchezizq@gmail.com>
2021-02-03 09:48:17 -0600

    Notes/Show vue page to show detail of a note

---
+++ resources/js/Pages/Notes/Show.vue
@@ -0,0 +1,49 @@
+<template>
+    <app-layout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Módulo de notas
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="md:grid md:grid-cols-3 md:gap-6">
+                    <div class="md:col-span-1">
+                        <div class="px-4 sm:px0">
+                            <h3 class="text-lg text-gray-900">Detalle</h3>
+                            <p class="text-sm text-gray-600">{{ note.excerpt }}</p>
+                        </div>
+                    </div>
+                    <div class="md:col-span-2 mt-5 md:mt-0">
+                        <div class="shadow bg-white md:rounded-md p-4">
+                           <p>
+                               {{ note.content }}
+                           </p>
+                           <hr class="my-6">
+
+                           <inertia-link :href="route('notes.index')">
+                               Volver
+                           </inertia-link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+    import AppLayout from '@/Layouts/AppLayout'
+    import Welcome from '@/Jetstream/Welcome'
+
+    export default {
+        components: {
+            AppLayout,
+            Welcome,
+        },
+        props: {
+            note: Object, //Simple objeto, osea un unico registro a diferencia de un arreglo de objetos
+        }
+    }
+</script>
