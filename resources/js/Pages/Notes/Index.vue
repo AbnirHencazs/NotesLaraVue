3297ad60b1ab89e3577778cfc7bcbf128bb9f808
BrianSanchez@Accesa <briansanchezizq@gmail.com>
2021-02-03 13:04:30 -0600

    Configuration for input text box to send its data to notecontroller

--- resources/js/Pages/Notes/Index.vue
+++ resources/js/Pages/Notes/Index.vue
@@ -17,9 +17,11 @@
                     </div>
                     <div class="md:col-span-2 mt-5 md:mt-0">
                         <div class="shadow bg-white md:rounded-md p-4">
-                            <div class="mb-4">
+                            <div class="flex justify-between">
+                                <input v-model="q" type="text" class="form-input rounded-md shadow-sm" placeholder="Buscar..."/>
                                 <inertia-link :href="route('notes.create')" class="bg-blue-500 text-white font-bold py-2 px-4 rounded-md">+</inertia-link>
                             </div>
+                            <hr class="my-5">
                             <table>
                                 <tr v-for="note in notes" :key="note.id">
                                     <td class="border px-4 py-2">
@@ -56,6 +58,18 @@
         },
         props: {
             notes: Array,
+        },
+        data() {
+            return {
+                q: ''
+            }
+        },
+        watch: {
+            q: function (value) {
+                this.$inertia.replace( this.route('notes.index', {
+                    q: value
+                }) );
+            }
         }
     }
 </script>
