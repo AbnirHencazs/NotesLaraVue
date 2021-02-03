ff7122e2ed3dbf659e5cf84442a2f5fee7394bad
BrianSanchez@Accesa <briansanchezizq@gmail.com>
2021-02-03 08:42:21 -0600

    dashboard method for PageController

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class PageController extends Controller
+{
+    public function dashboard()
+    {
+        return Inertia::render('Dashboard');
+    }
+}
