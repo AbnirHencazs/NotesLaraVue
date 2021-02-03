8f524a2392424f8c1eb1061e472a89f21117b9e6
BrianSanchez@Accesa <briansanchezizq@gmail.com>
2021-02-03 08:41:05 -0600

    Vue home page rendered from Mix through web route

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+        soy la mera verga
+    </div>
+</template>
