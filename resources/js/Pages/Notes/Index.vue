944866e2a6360728b6da687c70d3f51c1aeea3b7
BrianSanchez@Accesa <briansanchezizq@gmail.com>
2021-02-03 09:37:36 -0600

    Primitive listing of all notes in notes/index vue page

--- resources/js/Pages/Notes/Index.vue
+++ resources/js/Pages/Notes/Index.vue
@@ -15,6 +15,27 @@
                             <p class="text-sm text-gray-600">Toma el registro correcto y ejecuta cualquier función (ver, editar o eliminar)</p>
                         </div>
                     </div>
+                    <div class="md:col-span-2 mt-5 md:mt-0">
+                        <div class="shadow bg-white md:rounded-md p-4">
+                            <table>
+                                <tr v-for="note in notes" :key="note.id">
+                                    <td class="border px-4 py-2">
+                                        {{ note.excerpt }}
+                                    </td>
+                                    <td class="px-4 py-2">
+                                        <intertia-link :href="route('notes.show', note.id)">
+                                            Ver
+                                        </intertia-link>
+                                    </td>
+                                    <td class="px-4 py-2">
+                                        <intertia-link :href="route('notes.edit', note.id)">
+                                            Editar
+                                        </intertia-link>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
