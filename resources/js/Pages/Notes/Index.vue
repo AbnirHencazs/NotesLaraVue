569ca73642a64fa8693d9d6f2c9f7df6a2b97da6
BrianSanchez@Accesa <briansanchezizq@gmail.com>
2021-02-03 09:29:06 -0600

    New vue page Index for notes

---
+++ resources/js/Pages/Notes/Index.vue
@@ -0,0 +1,34 @@
+<template>
+    <app-layout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Módulo de notas
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="md:grid md:grid-cols-3 md:gap-6">
+                    <div class="md:col-span-1">
+                        <div class="px-4 sm:px0">
+                            <h3 class="text-lg text-gray-900">Listado de notas</h3>
+                            <p class="text-sm text-gray-600">Toma el registro correcto y ejecuta cualquier función (ver, editar o eliminar)</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+    import AppLayout from '@/Layouts/AppLayout'
+    import Welcome from '@/Jetstream/Welcome'
+
+    export default {
+        components: {
+            AppLayout,
+            Welcome,
+        },
+    }
+</script>
